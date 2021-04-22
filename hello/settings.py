3aa3e0bb35848dd3210598654f55d5cd4e6aaf5c
Pravin Vargad <63777884+pravinv1998@users.noreply.github.com>
2021-04-22 11:05:49 +0530

    error resolved

--- hello/settings.py
+++ hello/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd-3as979spomqu3&k40c6za7@$un#mja9&p_9b-7!w80u!_pu='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
